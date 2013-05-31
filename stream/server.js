487416bd6902803f10d8a18f468aadb7d4ff5690
John Todd <jqt3of5@linux.hsd1.ut.comcast.net>
2013-05-30 22:52:35 -0600

    improved the stream design.

--- stream/server.js
+++ stream/server.js
@@ -1,21 +1,34 @@
 var net = require('net');
+var spawn = require('child_process').spawn;
+var readline = require('readline');
 
 
+var capture = spawn('../hwi/capture');
+var rlInput = readline.createInterface({
+    input: capture.stdout, 
+    output: capture.stdin
+});
 
 var server = net.createServer(function(con) {
-
-
     console.log("A client has connected");
-    process.stdin.resume();
-    process.stdin.on('data', function(chunk) {
-	con.write(chunk);
 
+    rlInput.on('line', function(line) {
+	con.write(line);
     });
+    // capture.stdout.on('data', function (data) {
+    // 	con.write(data);
+    // });
 
-    process.stdin.on('end', function(chunk) {
-	process.stdout.write("end");
-	con.write("end");
-    });
+    // process.stdin.resume();
+    // process.stdin.on('data', function(chunk) {
+    // 	con.write(chunk);
+
+    // });
+
+    // process.stdin.on('end', function(chunk) {
+    // 	process.stdout.write("end");
+    // 	con.write("end");
+    // });
 });
 
 server.listen(1234, function() {});
